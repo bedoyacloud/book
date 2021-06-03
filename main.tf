46da881e51f437f38e4c13669e319a5f5d766da3
Carlos Bedoya <carloshernanbedoya@gmail.com>
2021-06-02 22:50:47 -0300

    uploading main

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-077e31c4939f6a2f3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
